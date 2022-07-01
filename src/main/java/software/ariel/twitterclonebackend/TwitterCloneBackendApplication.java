65cb57bfd2a004f35d78bc6cd04b468505bdecb0
Joseth Ariel <arielbaquero.ab@gmail.com>
2022-07-01 15:32:56 -0500

    initial commit

---
+++ src/main/java/software/ariel/twitterclonebackend/TwitterCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package software.ariel.twitterclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/software/ariel/twitterclonebackend/TwitterCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package software.ariel.twitterclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
